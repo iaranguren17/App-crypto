01274dca1c7a39a38db6082130330593721aeacc
iaranguren17 <iarangurenripoll@gmail.com>
2024-09-19 16:46:39 +0200

    venv

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #Este es el main bueno
+
+
+
+from cryptography.fernet import Fernet
+# Put this somewhere safe!
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+token
+b'...'
+f.decrypt(token)
+b'A really secret message. Not for prying eyes.'
